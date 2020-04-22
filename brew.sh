74942215b81372e72b5c7eaa0bb934a2117a7d8f
Juan Emilio González <jotae85@gmail.com>
2020-04-22 23:51:46 +0100

    Initial commit

---
+++ brew.sh
@@ -0,0 +1,37 @@
+#!/bin/zsh
+
+# Install Xcode from the App Store before running this script
+
+echo "Starting bootstrapping..."
+
+if test ! $(which brew); then
+    echo "Installing Hombrew..." 
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+fi
+
+brew update
+
+PACKAGES=(
+    swiftlint
+    loc
+    tmux
+    tree
+)
+
+brew install ${PACKAGES[@]}
+brew cleanup
+
+CASKS=(
+    google-chrome
+    virtualbox
+    sourcetree
+    alfred
+    appcleaner
+    dropbox
+    paw
+    daisydisk
+    visual-studio-code
+    1password
+)
+
+brew cask install ${CASKS[@]}
